194c092ac7fbc218202941e866cc8a845a22717a
Purushotham <purusothamyb@gmail.com>
2019-11-14 19:18:22 +0530

    Added register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
+import {UserLoginComponent} from './user-login/user-login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user/login',
+    component: UserLoginComponent
+  },
+  {
+    path: 'user/login/dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'user/login/register',
+    component: UserRegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    DashboardComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
